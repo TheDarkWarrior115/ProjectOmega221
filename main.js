e4af634000a4496fd4127b9634a3b061d2a264b2
TheDarkWarrior115 <115947724+TheDarkWarrior115@users.noreply.github.com>
2023-04-05 21:52:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-a=()=>{location.reload()};if(id){a()}else{const id=navigator.userAgent}import (bans) from "list.js";for(c in bans) {if(id===bans[c]){document.write("You Have Been Banned")}else{}}
+a=()=>{location.reload()};if(id){a()}else{const id=navigator.userAgent}import (bans) from "bans.js";for(c in bans){if(id===bans[c]){document.write("You Have Been Banned")}else{import(ids,perms) from "perms.js";for(c in ids){if(id===ids[c]){import("perm-"+perms[c]+".js")}else{import("perms-basic.js")}}}}
