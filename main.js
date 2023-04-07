c4caa5308ed48c22f36e46dd97dd210634250374
TheDarkWarrior115 <115947724+TheDarkWarrior115@users.noreply.github.com>
2023-04-06 19:54:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-if(id){location.reload()}else{const id=navigator.userAgent}import (bans) from "bans.js";for(c in bans){if(id===bans[c]){document.write("You Have Been Banned")}else{import("perms-basic.js")}
+
