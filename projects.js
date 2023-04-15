771f294e7c559a97f4a02b9ec0e7f934902dcae3
TheDarkWarrior115 <115947724+TheDarkWarrior115@users.noreply.github.com>
2023-04-14 22:59:48 -0600

    Delete projects.js

--- projects.js
+++
@@ -1,8 +0,0 @@
-install = function(x){localStorage.setItem("projects",localStorage.getItem("projects")+=x)}
-name = [];
-author = [];
-desc = [];
-a = "projectarray = [";
-for(i in name){
-	a+="<h3>"+name[i]+"</h3><br>"+"<span><i>"+author+"</i><br><p>"+desc+"</p><span/><br><button onclick='install("++")'>Install</button>"
-}
