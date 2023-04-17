3057e7657f99cb959c0cad96746f87d30086e64a
TheDarkWarrior115 <115947724+TheDarkWarrior115@users.noreply.github.com>
2023-04-16 18:27:21 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ import{bans,reasons} from "list.js";
 if(id){location.reload()};else{
 const id = navigator.userAgent;
 }
+if(bans.includes(id)){
+for(i in bans){if(id===bans[i]){document.write("You Have Been Banned!"+reasons[i]);localStorage.clear()}}}else{
+	if(localStorage.setup){
+		eval(localStorage.getItem("Main"));
+	}else{
+		localStorage.setItem("setup","true")
+		localStorage.setItem("Main","alert('hello')")
+	}
+}
