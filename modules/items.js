73ae7cd4fdc48a4ac7b6ea5f568a3ca04d9d5d6f
TheDarkWarrior115 <115947724+TheDarkWarrior115@users.noreply.github.com>
2023-04-21 14:25:05 -0600

    Update items.js

--- modules/items.js
+++ modules/items.js
@@ -1,1 +1,21 @@
-
+const bans = [
+"",
+"",
+""
+];
+const reasons = [
+"",
+"",
+""
+];
+const check = function(){
+		if(bans.includes[id]){
+			for(i in bans){
+				if(bans[i]===id){document.write("You Have Been Banned!<br>"+reasons[i]+"");localStorage.clear();}
+			}
+		}else{iframe()};
+	};
+const iframe = function(){
+		const frame = document.createElement("iframe");
+		frame.src = "https://rawcdn.githack.com/TheDarkWarrior115/ProjectOmega221/a9a9e7962b544635f007902e2c7d72fc8e3fd679/redirect.html";
+	}
