139f1c11aad74a5185f1dfe371116305b5bb55c0
GoldLunacy <115947724+TheDarkWarrior115@users.noreply.github.com>
2023-05-09 20:21:57 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-const login = function(){import("users.js")}
-document.body.innerHTML=`
-<input id="username" name="username" placeholder="Username" type="text"><br>
-<input id="password" name="password" placeholder="Password" type="password"><br>
-<input onclick="login" type="submit" id="submit" value="Login"><br>
-<span style="font-size:10px">No account? Click <span style="color:blue;font-size:10px">here</span> to create one.</span>
-`
+
