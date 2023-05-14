8aedb9c14931a0a9d31b5d167a5d86a70e408ea0
GoldLunacy <115947724+TheDarkWarrior115@users.noreply.github.com>
2023-05-13 19:15:10 -0600

    Update setup_login.js

--- setup_login.js
+++ setup_login.js
@@ -1,9 +1,1 @@
-if(navigator.cookieEnabled!==true){
-		document.write("Cookies Are Not Enabled Go To Settings To Enable.")
-	}else if(navigator.onLine!==true){
-		document.write("You Are Not Connected To A Network");
-	}else{
-		localStorage.setItem("login",`
-		document.body.innerHTML="<form><input id='username' name='username' type='text' placeholder='Username'><br><input id='password' name='password' type='password' placeholder="Password"><input id='login' name='login' type='submit'></form><br><span>Don't Have An Account Or Want Another One? Click<span onclick=''> Here</span> To Create One<br>Forgot Password? Click <span>Here</span> To Set</span>"
-		`)
-	}
+
